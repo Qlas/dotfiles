bf85e2444e74600ca42de569a84a8306474d632f
Qlas <rafgor7@gmail.com>
2023-05-24 22:16:30 +0200

    added formatter and linter for lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,21 +1,22 @@
-lvim.builtin.treesitter.ensure_installed = {
-  "python",
-}
+lvim.builtin.treesitter.ensure_installed = {"python"}
 
-lvim.format_on_save = {
-  enabled = true
-}
+lvim.format_on_save = {enabled = true}
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 
 formatters.setup {
-  { command = "black", filetypes = { "python" } },
-  { command = "isort", filetypes = { "python" } },
-  -- { command = "mypy",  filetypes = { "python" } },
+    {command = "black", filetypes = {"python"}},
+    {command = "isort", filetypes = {"python"}},
+    {command = "lua-format", filetypes = {"lua"}}
 
 }
 
 local linters = require "lvim.lsp.null-ls.linters"
 
-linters.setup { { command = "flake8", filetypes = { "python" } }, { command = "mypy", filetypes = { "python" } }
+linters.setup {
+    {command = "flake8", filetypes = {"python"}}, {
+        command = "mypy",
+        filetypes = {"python"},
+        {command = "luacheck", filetypes = {"lua"}}
+    }
 }
