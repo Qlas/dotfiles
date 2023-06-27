5d8a607656a4f3a5a2c2232d2e95a512740de88f
Rafał Górski <r.gorski@satrev.space>
2023-06-27 10:28:43 +0200

    relative numbers

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -24,8 +24,12 @@ lvim.plugins = {
     }
 }
 
+vim.opt.relativenumber = true
+
 -- Setting nvim-notify as default vim notification system
 vim.notify = require("notify")
+
+require("notify").setup({background_colour = "#1a1b26"})
 
 -- automatically install python syntax highlighting
 lvim.builtin.treesitter.ensure_installed = {"python"}
