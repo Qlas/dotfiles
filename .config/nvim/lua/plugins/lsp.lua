b2ce99c81571d409c7e968bf9842467f57fa150b
Qlas <rafgor7@gmail.com>
2025-04-09 08:29:16 +0200

    add basepyright for inline python hints

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -10,37 +10,14 @@ lspconfig.basedpyright.setup({
           genericTypes = true,
         },
       },
-    },
-  },
-  on_attach = function(client, _)
-    client.server_capabilities.hoverProvider = false
-    client.server_capabilities.definitionProvider = false
-  end,
-})
-
-return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pyright = {
-          settings = {
-            pyright = {
-              -- Using Ruff's import organizer
-              disableOrganizeImports = true,
-            },
-            python = {
-              analysis = {
-                -- Ignore all files for analysis to exclusively use Ruff for linting
-                ignore = { "*" },
-              },
-            },
-          },
-        },
-      },
       on_attach = function(client)
         client.server_capabilities.documentFormattingProvider = false -- Disable formatting
       end,
     },
   },
+})
+
+return {
+  "neovim/nvim-lspconfig",
+  opts = { servers = { pyright = { enabled = false } } },
 }
