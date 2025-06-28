ea987b726463c077acf013bc90d2798153337056
Qlas <rafgor7@gmail.com>
2025-06-28 18:56:29 +0200

    rustaceanvim configuration

---
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -0,0 +1,12 @@
+return {
+  "mrcjkb/rustaceanvim",
+  opts = {
+    settings = {
+      ["rust-analyzer"] = {
+        procMacro = {
+          enable = true,
+        },
+      },
+    },
+  },
+}
